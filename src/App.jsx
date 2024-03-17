d316354ac1c3f0734432a28f8fa8200334aace3b
okonpatrick <okonpatrick2000@gmail.com>
2024-03-17 14:16:38 -0700

    couple of changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,18 @@ import LoginForm from "../src/components/Forms/LoginForm";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VendorsDashboard from "./components/Vendors/VendorsDashboard";
 import AdminDashboard from "./components/Admin/AdminDashboard";
+import AdminSideBar from "./components/Admin/AdminSideBar";
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <>
         <Routes>
         <Route path="/login" element={<LoginForm />} />
           <Route path="/dashboard" element={<div><NavBar /> <VendorsDashboard />  </div>} />
-          <Route path="/admin" element={<AdminDashboard />} />    
+          <Route path="/adminpanel" element={<AdminDashboard />} />    
           {/* Other routes go here */}
         </Routes>
-      </>
     </Router>
   );
 }
